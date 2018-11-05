61e519089da108ec1daf6b675625414be170f49a
sebgiles <mrsebgiles@gmail.com>
2018-11-05 19:22:41 +0100

    Revert "cube aggiunta spi + fatfs"
    This reverts commit 15a308ae743814ff39428feb7d07520d93f1444c.

--- Src/main.c
+++ Src/main.c
@@ -1,5 +1,6 @@
 
 /**
+<<<<<<< HEAD
  ******************************************************************************
  * @file           : main.c
  * @brief          : Main program body
@@ -46,12 +47,48 @@
  *
  ******************************************************************************
  */
+=======
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+>>>>>>> parent of 15a308a... cube aggiunta spi + fatfs
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f3xx_hal.h"
 #include "can.h"
-#include "fatfs.h"
-#include "spi.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -121,6 +158,7 @@ int main(void)
 
 	/* USER CODE END SysInit */
 
+<<<<<<< HEAD
 	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
 	MX_USART2_UART_Init();
@@ -128,6 +166,13 @@ int main(void)
 	MX_SPI1_Init();
 	MX_FATFS_Init();
 	/* USER CODE BEGIN 2 */
+=======
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_CAN_Init();
+  /* USER CODE BEGIN 2 */
+>>>>>>> parent of 15a308a... cube aggiunta spi + fatfs
 
 	// per usare printf ...
 	initialise_monitor_handles();
